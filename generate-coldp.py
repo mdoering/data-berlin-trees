50d35d93f7ddafbadd4b3871be0360f2536c216a
Markus Döring <mdoering@gbif.org>
2025-12-27 23:27:00 +0100

    Replace line returns in location data

--- generate-coldp.py
+++ generate-coldp.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import json
+import json, re
 from pyproj import Transformer
 from datetime import date
 
@@ -11,6 +11,10 @@ current_year = date.today().year
 # coordinate transformer
 transformer = Transformer.from_crs("EPSG:25833", "EPSG:4326", always_xy=True)
 
+IDs=set()
+
+def clean(x):
+    return re.sub('[\n\r\t]',' ', x)
 
 def writeRec(out, rec):
     props   = rec["properties"]
@@ -18,6 +22,10 @@ def writeRec(out, rec):
     if not sn or sn=='Unbekannt' or sn=='None':
         return
     ID = rec["id"]
+    IDlow=ID.lower()
+    if IDlow in IDs:
+        print("Non unique ID: %s" % ID)
+    IDs.add(IDlow)
     yearStr = props["pflanzjahr"]
     year = int(yearStr) if yearStr else 0
 
@@ -31,10 +39,10 @@ def writeRec(out, rec):
     lat = coord[1]
     dp = "{\"age\":%s, \"trunk diameter\":%s, \"height\":%s, \"planting year\":%s}" % (props["standalter"], props["stammumfg"], props["baumhoehe"], props["pflanzjahr"])
     # record for this year
-    out.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\t%s\n" % (ID, props["kennzeich"], sn, props["art_dtsch"], props["gattung"], props["bezirk"], props["namenr"], lat, lon, vlat, vlon, current_year, dp))
+    out.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\t%s\n" % (ID, props["kennzeich"], sn, props["art_dtsch"], props["gattung"], clean(props["bezirk"]), clean(props["namenr"]), lat, lon, vlat, vlon, current_year, dp))
     # do another record for the year it was planted
     if year > 1800 and year < current_year:
-        out.write("%s.planted\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\"Year the tree was planted\"\t\n" % (ID, props["kennzeich"], sn, props["art_dtsch"], props["gattung"], props["bezirk"], props["namenr"], lat, lon, vlat, vlon, year))
+        out.write("%s.planted\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\"Year the tree was planted\"\t\n" % (ID, props["kennzeich"], sn, props["art_dtsch"], props["gattung"], clean(props["bezirk"]), clean(props["namenr"]), lat, lon, vlat, vlon, year))
 
 
 
