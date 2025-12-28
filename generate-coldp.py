f4a53cbdbe4efb58399bffa3270ddd8bbb8496b1
Markus Döring <mdoering@gbif.org>
2025-12-28 10:17:08 +0100

    Add individuals and allow trees up to 1000 years old

--- generate-coldp.py
+++ generate-coldp.py
@@ -11,8 +11,6 @@ current_year = date.today().year
 # coordinate transformer
 transformer = Transformer.from_crs("EPSG:25833", "EPSG:4326", always_xy=True)
 
-IDs=set()
-
 def clean(x):
     return re.sub('[\n\r\t]',' ', x)
 
@@ -22,10 +20,6 @@ def writeRec(out, rec):
     if not sn or sn=='Unbekannt' or sn=='None':
         return
     ID = rec["id"]
-    IDlow=ID.lower()
-    if IDlow in IDs:
-        print("Non unique ID: %s" % ID)
-    IDs.add(IDlow)
     yearStr = props["pflanzjahr"]
     year = int(yearStr) if yearStr else 0
 
@@ -38,17 +32,19 @@ def writeRec(out, rec):
     lon = coord[0]
     lat = coord[1]
     dp = "{\"age\":%s, \"trunk diameter\":%s, \"height\":%s, \"planting year\":%s}" % (props["standalter"], props["stammumfg"], props["baumhoehe"], props["pflanzjahr"])
+    data = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (ID, props["kennzeich"], sn, props["art_dtsch"], props["gattung"], clean(props["bezirk"]), clean(props["namenr"]), lat, lon, vlat, vlon)
     # record for this year
-    out.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\t%s\n" % (ID, props["kennzeich"], sn, props["art_dtsch"], props["gattung"], clean(props["bezirk"]), clean(props["namenr"]), lat, lon, vlat, vlon, current_year, dp))
-    # do another record for the year it was planted
-    if year > 1800 and year < current_year:
-        out.write("%s.planted\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\"Year the tree was planted\"\t\n" % (ID, props["kennzeich"], sn, props["art_dtsch"], props["gattung"], clean(props["bezirk"]), clean(props["namenr"]), lat, lon, vlat, vlon, year))
-
+    out.write("%s\t%s\t%s\t%s\t%s\n" % (ID, data, current_year, "", dp))
+    # do another record for the year it was planted. The oldest tree in Berlin is "Die alte Marie", a 860 year old oak
+    if year > 1000 and year < current_year:
+        out.write("%s.planted\t%s\t%s\t%s\t%s\n" % (ID, data, year, "Year the tree was planted", ""))
+    elif year < 2025 and year > 0:
+        print("Ignore planting year %s" % (year))
 
 
 
 with open(outFile, 'w', newline='') as out:
-    out.write("occurrenceID\tcatalogNumber\tscientificName\tvernacularName\tgenus\tmunicipality\tlocality\tdecimalLatitude\tdecimalLongitude\tverbatimLatitude\tverbatimLongitude\tyear\toccurrenceRemarks\tdynamicProperties\n")
+    out.write("occurrenceID\torganismID\tcatalogNumber\tscientificName\tvernacularName\tgenus\tmunicipality\tlocality\tdecimalLatitude\tdecimalLongitude\tverbatimLatitude\tverbatimLongitude\tyear\toccurrenceRemarks\tdynamicProperties\n")
     for fn in files:
         print("Process file "+fn)
         with open(fn) as f:
